2fe3af8de9e7da5e9eac8e5b89c628cb332da839
Rahul Shah <rshahshah2890@gmail.com>
2021-09-03 19:09:55 +0530

    Vanilla chrome extension

---
+++ js/background.js
@@ -0,0 +1,5 @@
+let color = '#3aa757';
+
+chrome.runtime.onInstalled.addListener(() => {
+    console.log('Default background color set to %cgreen', `color: ${color}`);
+});
---
+++ popup.js
@@ -0,0 +1,6 @@
+// Initialize button with user's preferred color
+let changeColor = document.getElementById("changeColor");
+
+changeColor.addEventListener("click", () => {
+    alert("Clicked");
+});
