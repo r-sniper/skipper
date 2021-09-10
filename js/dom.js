c67e815982023b6f3d4ba97ad40fa4cf2a754c6b
Rahul Shah <rshahshah2890@gmail.com>
2021-09-10 13:41:13 +0530

    * Removed youtube.js reference * Added enums * Added comment and todo

--- js/dom.js
+++ js/dom.js
@@ -1,19 +1,36 @@
+const CURRENT_TIME_STATE = {
+    BEFORE: "BEFORE",
+    BETWEEN: "BETWEEN",
+    AFTER: "AFTER",
+}
+
 var skippingEnabled = false;
 var currentSkipIndex = 0;
-var lines = [];
-var nextLine;
+var skipLines = [];
+var nextSkipLine;
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     console.log(request);
     if (request.requestType == "timestamps") {
         // console.log("message received");
         console.log(request.lines);
-        lines = request.lines;
-        nextLine = getNextSkippingTimestamp();
+        skipLines = request.lines;
+        nextSkipLine = getNextSkippingTimestamp();
         skippingEnabled = true;
-        sendResponse({ dom: "received" });
+        sendResponse({dom: "received"});
     }
 });
+
+const getCurrentTimeState = function (startTime, endTime, currentTime) {
+    if (currentTime < startTime) {
+        return CURRENT_TIME_STATE.BEFORE;
+    }
+    if (currentTime >= endTime) {
+        return currentTime.AFTER;
+    }
+    return currentTime.BETWEEN;
+}
+
 
 window.onload = () => {
     let videoPlayer = document.getElementsByClassName('video-stream html5-main-video')[0];
@@ -22,18 +39,28 @@ window.onload = () => {
         let currentTime = videoPlayer.currentTime;
         if (skippingEnabled) {
             if (skipped) {
-                nextLine = getNextSkippingTimestamp();
+                nextSkipLine = getNextSkippingTimestamp();
                 skipped = false;
             }
-            let startTime = nextLine.start.timeInSeconds;
-            let endTime = nextLine.end.timeInSeconds;
-            console.log("currentTime: " + currentTime + " start: " + startTime+ " end: " + endTime);
-            if (currentTime >= startTime && currentTime <= endTime) {
+            let startTime = nextSkipLine.start.timeInSeconds;
+            let endTime = nextSkipLine.end.timeInSeconds;
+            console.log("currentTime: " + currentTime + " start: " + startTime + " end: " + endTime);
+
+            let currentTimeSate = getCurrentTimeState(startTime, endTime, currentTime);
+
+            if (currentTimeSate == CURRENT_TIME_STATE.BETWEEN) {
                 videoPlayer.currentTime = endTime;
                 skipped = true;
-                console.log("skipped");
             }
-            // console.log(getNextSkippingTimestamp());
+            /* TODO: This is a scenario where user manually seeks the video forward
+            * This would not work in the below mentioned scenarios
+            * 1. If user goes back in time, skipping would not work
+            * 2. If user goes too forward in time, might miss some skipping
+            * Suggestion, get the exact skipping line based on the current timestamp
+            * */
+            if (currentTimeSate == CURRENT_TIME_STATE.AFTER) {
+                nextSkipLine = getNextSkippingTimestamp();
+            }
 
         }
     });
@@ -41,7 +68,7 @@ window.onload = () => {
 
 
 const getNextSkippingTimestamp = function () {
-    let nextLine = lines[currentSkipIndex];
+    let nextLine = skipLines[currentSkipIndex];
     currentSkipIndex++;
     return nextLine;
 }
